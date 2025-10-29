6cd94352d4e5e018ebf8bf2fdbef1d7f9ba93e0b
Himanshu Chaudhary <himanshuch6174@gmail.com>
2025-10-29 14:20:07 +0200

    Update WangWangCMB.py

--- simplemc/likelihoods/WangWangCMB.py
+++ simplemc/likelihoods/WangWangCMB.py
@@ -44,6 +44,7 @@ class PLKLikelihood(WangWangCMB):
             cov = [[1.0, 0.5250, -0.4235],
                    [0.5250, 1.0, -0.6925],
                    [-0.4235, -0.6925,  1.0]]
+            
         elif matrices == "PLA1":
             # base_omegak
             mean = [3.01510344e+02,   1.74340204e+00,   2.23128506e-02]
@@ -51,6 +52,7 @@ class PLKLikelihood(WangWangCMB):
             cov = [[1.,          0.57063287, -0.48438085],
                    [0.57063287,  1.,         -0.67948316],
                    [-0.48438085, -0.67948316,  1.]]
+            
         elif matrices == "PLA2":
             # base_nrun_r_omegak
             mean = [3.01412183e+02, 1.73538851e+00, 2.27716863e-02]
@@ -58,6 +60,7 @@ class PLKLikelihood(WangWangCMB):
             cov = [[1.,          0.66889481, -0.59554585],
                    [0.66889481,  1.,         -0.78180126],
                    [-0.59554585, -0.78180126,  1.]]
+            
         elif matrices == "PLA3":
             # base_nrun_r_omegak
             mean = [3.01539311e+02,   1.74066358e+00,   2.25879526e-02]
@@ -65,20 +68,38 @@ class PLKLikelihood(WangWangCMB):
             cov = [[1.,  0.62040041, -0.53470863],
                    [0.62040041,  1., -0.71007217],
                    [-0.53470863, -0.71007217,  1.]]
-        elif matrices == 'PLK18':
-            # LCDM arXiv:1808.05724
+            
+        elif matrices == 'WW_ΛCDM':
+            # ΛCDM arXiv:1808.05724 (Calibrated with Planck TT,TE,EE + lowE )
             mean = [3.01471e+02, 1.7502, 2.236e-02]
             err = [0.089, 0.0046, 0.00015]
-            cov = [[1., 0.46, -0.33],
-                   [0.46, 1., -0.66],
+            cov = [[1.0, 0.46, -0.33],
+                   [0.46, 1.0, -0.66],
                    [-0.33, -0.66, 1.0]]
-        elif matrices == 'PLK18_w':
-            # wCDM arXiv:1808.05724
+            
+        elif matrices == 'WW_wCDM':
+            # wCDM arXiv:1808.05724 (Calibrated with Planck TT,TE,EE + lowE )
             mean = [3.01462e+02, 1.7493, 2.239e-02]
-            err = [0.089, 0.0046, 0.00015]
-            cov = [[1., 0.54, -0.42],
-                   [0.54, 1., -0.75],
-                   [-0.42, -0.75, 1.0]]
+            err  = [0.089, 0.0046, 0.00015]
+            cov  = [[1.0,   0.47,  -0.34],
+                    [0.47,  1.0,   -0.66],
+                    [-0.34, -0.66,  1.0]]
+            
+        elif matrices == 'WW_ΛCDM_OK':
+            # ΛCDM + Ω_k  arXiv:1808.05724 (Calibrated with Planck TT,TE,EE + lowE )
+            mean = [3.01409e+02, 1.7429, 2.260e-02]
+            err  = [0.091, 0.0051, 0.00017]
+            cov  = [[1.0,     0.54,   -0.42],
+                    [0.54,    1.0,    -0.75],
+                    [-0.42,  -0.75,    1.0]]
+            
+        elif matrices == 'WW_LCDM_AL':
+            # ΛCDM + A_L  arXiv:1808.05724 (Calibrated with Planck TT,TE,EE + lowE )
+            mean = [3.01406e+02, 1.7428, 2.259e-02]
+            err  = [0.090, 0.0053, 0.00017]
+            cov  = [[1.0,   0.52,  -0.41],
+                    [0.52,  1.0,   -0.72],
+                    [-0.41, -0.72,  1.0]]
         else:
             print("Bad matrices param")
             sys.exit(1)
@@ -94,6 +115,7 @@ class WMAPLikelihood(WangWangCMB):
             cov = [[1.0, 0.3883, -0.6089],
                    [0.3883, 1.0, -0.5239],
                    [-0.6089, -0.5239,  1.0]]
+            
         elif matrices == "PLA":
             mean = [3.01969812e+02,   1.73081611e+00,   2.26651563e-02]
             err = [6.61780337e-01,   1.60432146e-02,   5.18589026e-04]
